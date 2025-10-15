29cc58064992a4d2d71051b9dfe5a13424c5fa78
Igor Stojanovic <igor.stojanovic.nis@outlook.com>
2025-10-15 22:04:31 +0200

    Initial content branch

--- src/config/translationsUtil.jsx
+++
@@ -1,7 +0,0 @@
-import i18next from "i18next";
-
-export default function getTranslation(messageCode, params = {}) {
-  var translation = "";
-  translation = i18next.t(messageCode, params);
-  return translation;
-}
--- src/pages/CareersPage.jsx
+++
@@ -1,5 +0,0 @@
-const CareersPage = () => {
-  return <>Careers Page</>;
-};
-
-export default CareersPage;
